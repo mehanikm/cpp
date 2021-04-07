055be3d2a5c08397a585f267b754c8c07018caa3
Mykhailo Marchenko <mehanikm@gmail.com>
2021-04-07 10:37:34 +0200

    Update ZadParzysta

--- kccpZadania/ZadParzysta.cpp
+++ kccpZadania/ZadParzysta.cpp
@@ -8,8 +8,19 @@ int main()
     cout << "Number to check: ";
     cin >> a;
 
-    if (a % 2 == 1)
+    cout << "Bit operator" << endl;
+    // checking e.g. for 10 -> 10 == 0b1010 -> 0b1010 & 0b0001 == 0b0000 == 0; e.g. for 13 -> 13 == 0b1101 -> 0b1101 & 0b0001 == 0b0001 == 1;
+    if (a & 1)
         cout << "Number is odd" << endl;
     else
         cout << "Number is even" << endl;
+
+    cout << "Modulus operator" << endl;
+    if (a % 2)
+        cout << "Number is odd" << endl;
+    else
+        cout << "Number is even" << endl;
+
+    cout << "Ternary operator" << endl;
+    cout << ((a % 2) ? "Number is odd" : "Number is even") << endl;
 }
